5399c7dee3a818d89ddfc793181432af146e3be9
Froggy <froggy.happy.f@gmail.com>
2022-01-07 00:13:08 +0300

    Расширил applications

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	_ "github.com/XFroggyX/InteractionGOandPSQL/pkg/models/postgresql"
 	_ "github.com/jackc/pgx"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"log"
@@ -12,14 +13,15 @@ import (
 )
 
 type application struct {
-	errorLog *log.Logger
-	infoLog  *log.Logger
+	errorLog  *log.Logger
+	infoLog   *log.Logger
+	countries *postgresql.CountriesModel
 }
 
 func main() {
 	port := flag.String("port", "4000", "Сетевой порт")
 	host := flag.String("addr", "127.0.0.1", "Сетевой адрес")
-	dbUrl := flag.String("db", "postgres://admin:admin@localhost:5432/Countries",
+	dbURL := flag.String("db", "postgres://admin:admin@localhost:5432/Countries",
 		"Название Postgresql базы данных")
 	flag.Parse()
 
@@ -27,7 +29,7 @@ func main() {
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	ctx := context.Background()
-	db, err := openDB(ctx, *dbUrl)
+	db, err := openDB(ctx, *dbURL)
 	if err != nil {
 		errorLog.Fatal(err)
 	}
@@ -49,8 +51,8 @@ func main() {
 	errorLog.Fatal(err)
 }
 
-func openDB(ctx context.Context, dbUrl string) (*pgxpool.Pool, error) {
-	dbPool, err := pgxpool.Connect(ctx, dbUrl)
+func openDB(ctx context.Context, dbURL string) (*pgxpool.Pool, error) {
+	dbPool, err := pgxpool.Connect(ctx, dbURL)
 	if err != nil {
 		return nil, err
 	}
--- pkg/models/postgresql/countries.go
+++ pkg/models/postgresql/countries.go
@@ -1,1 +1,19 @@
 package postgresql
+
+import (
+	model "github.com/XFroggyX/InteractionGOandPSQL/pkg/models"
+	"github.com/jackc/pgx/pgxpool"
+)
+
+type CountriesModel struct {
+	DB *pgxpool.Pool
+}
+
+func (m *CountriesModel) Insert(CountriesName string, Flag string, ReligionID int, LanguagesId int,
+	GovernmentFormID int, TerritorySizeID int) (int, error) {
+	return 0, nil
+}
+
+func (m *CountriesModel) Get(id int) (*model.Countries, error) {
+	return nil, nil
+}
