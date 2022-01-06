fee0bbe56dbd6ab3cc17964ab23f86d1468af3b9
Froggy <37781049+xfroggyx@users.noreply.github.com>
2022-01-06 20:27:47 +0300

    Закрыл достпук из вне к static файлам

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 const (
@@ -16,10 +17,35 @@ func main() {
 	mux.HandleFunc("/table", showTable)
 	mux.HandleFunc("/table/insert", insertTable)
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(customizableFileSystem{http.Dir("./static/")})
+	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Println("Server address: http://" + host + ":" + port)
 	err := http.ListenAndServe(host+":"+port, mux)
 	log.Fatal(err)
+}
+
+type customizableFileSystem struct {
+	fs http.FileSystem
+}
+
+func (cfs customizableFileSystem) Open(path string) (http.File, error) {
+	f, err := cfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "page.html")
+		if _, err := cfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+			return nil, err
+		}
+	}
+	return f, nil
 }
