f72b342afa84f1d6c0b41e6d7bf30f892c7785b0
Froggy <froggy.happy.f@gmail.com>
2022-01-07 00:06:06 +0300

    Добавил модель

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,1 +1,15 @@
 package models
+
+import "errors"
+
+var ErrNoRecord = errors.New("models: подходящей записи не найдено")
+
+type Countries struct {
+	ID               int
+	CountriesName    string
+	Flag             string
+	ReligionID       int
+	LanguagesId      int
+	GovernmentFormID int
+	TerritorySizeID  int
+}
